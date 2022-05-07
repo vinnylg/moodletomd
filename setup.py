ecfead57dbdf4fd2bf29e52e7fe7eca2f5b093df
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2022-05-07 11:31:47 -0300

    initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='moodletomd',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scrap any moodle and turn moodle classes into MD files.',
+    author='Vinícius de Lima Gonçalves',
+    license='MIT',
+)
